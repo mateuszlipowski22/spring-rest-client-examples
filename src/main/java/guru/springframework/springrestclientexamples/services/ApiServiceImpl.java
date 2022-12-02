bcf94d3a98df0cbd24d3ccd2dd9cdb809a4e6ff7
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-02 23:12:07 +0100

    Adding api.url=https://jsonplaceholder.typicode.com/users/ and UriComponentBuilder for ApiServiceImpl

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,9 +1,11 @@
 package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,15 +14,20 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService{
 
     private final RestTemplate restTemplate;
+    private final String API_URL;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String API_URL ){
         this.restTemplate = restTemplate;
+        this.API_URL=API_URL;
     }
 
     @Override
     public List<User> getUsers() {
 
-        ResponseEntity<User[]> response =restTemplate.getForEntity("https://jsonplaceholder.typicode.com/users/",User[].class);
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(API_URL);
+
+        ResponseEntity<User[]> response =restTemplate.getForEntity(uriComponentsBuilder.toUriString(),User[].class);
         return Arrays.asList(response.getBody());
     }
 }
